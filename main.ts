348e5ecc125308891d787451f425a6dc646621ba
hslotholt <henrik@slotholt.net>
2025-05-29 13:15:17 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace TestBlocks {
      * Plus to tal sammen
      */
     //% blockId=testblocksplus
-    //% block="plus"
+    //% block="plus $a $b"
     export function Plus(a: number, b:number) {
         return a + b;
     }
